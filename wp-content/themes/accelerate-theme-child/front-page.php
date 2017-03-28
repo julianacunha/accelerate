dc09ace918f26e6fc29e4cb91c1bc75e094334e2
Juliana <julianalimacunha@hotmail.com>
2017-03-28 15:18:27 -0500

    added plugin

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -1,11 +1,7 @@
 <?php
 /**
  * The template for displaying the homepage
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
@@ -29,6 +25,31 @@ get_header(); ?>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
+
+  <section class="featured work">
+      <div class="site-content">
+
+           <h4>Featured Work</h4>
+           
+  <ul class="homepage-featured-work">
+    <?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+           <?php while ( have_posts() ) : the_post();
+              $image_1 = get_field("image_1");
+              $size = "medium";
+           ?>
+
+            <li class="individual-featured-work">
+               <figure>
+                   <?php echo wp_get_attachment_image($image_1, $size); ?>
+                </figure>
+
+                   <h3><a href="<?php the_permalink(); ?>"<?php the_title(); ?></a></h3>
+            </li>
+          <?php endwhile; ?>
+          <?php wp_reset_query(); ?>
+   </ul>
+   </div>
+   </section>
 
 <section class="recent-posts">
 	<div class="site-content">
